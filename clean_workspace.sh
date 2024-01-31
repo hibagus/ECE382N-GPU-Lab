c1bbf72236677ce1f63367879b65a5ad007ce17f
Bagus Hanindhito <git@bagus.my.id>
2024-01-31 14:17:57 -0600

    add cleaning script just in case someone messed up

---
+++ clean_workspace.sh
@@ -0,0 +1,23 @@
+echo "Workspace Cleaner"
+echo "This will return the state of the workspace before you run prepare_workspace.sh"
+echo "This action is irreversible and will delete all of your generated data!!!"
+read -p "Are you sure (y/n)?" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Cleaning-up Workspace..."
+    deactivate
+    rm -rf nsight-compute/*
+    rm -rf application/val
+    rm -rf application/train
+    rm -rf application/test
+    rm -rf application/log
+    rm -rf application/checkpoint
+    rm -rf application/venv
+    cd kernel/build
+    make clean
+    rm -rf *
+    echo "Workspace is now clean."
+else
+    echo "Clean-up cancelled."
+fi
